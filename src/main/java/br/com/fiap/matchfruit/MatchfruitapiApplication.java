a770487525c952dd9fbab3b97d5b5432b72c1099
Luke Yoshioka <46505446+zurkon@users.noreply.github.com>
2021-09-26 14:11:22 -0300

    Updated for Jenkins Tomcat Deployment

--- src/main/java/br/com/fiap/matchfruit/MatchfruitapiApplication.java
+++ src/main/java/br/com/fiap/matchfruit/MatchfruitapiApplication.java
@@ -2,9 +2,16 @@ package br.com.fiap.matchfruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MatchfruitapiApplication {
+public class MatchfruitapiApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    	return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchfruitapiApplication.class, args);
